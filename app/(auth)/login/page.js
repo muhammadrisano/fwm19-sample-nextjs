340c9f27be576609903df026d64ce38da5f223bb
muhammad risano <muhammadrisano@gmail.com>
2024-05-27 18:22:01 +0700

    add autentication

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,10 +1,66 @@
+"use client"
+import { login } from '@/service/auth'
+import { useRouter } from 'next/navigation'
+// import React, {useRef, useEffect} from 'react'
 import React from 'react'
 
+
 const Login = () => {
+  const router = useRouter()
+  // const email = useRef(null)
+  // const password = useRef(null)
+
+  // const handleSubmit = (e)=>{
+  //   e.preventDefault()
+  //   const payload = {
+  //     email: email.current.value,
+  //     password: password.current.value
+  //   }
+  //   console.log(payload);
+  //   email.current.value = ""
+  //   password.current.value = ""
+  // }
+  
+  // useEffect(()=>{
+  //   email.current.focus()
+  // }, [])
+  const handleFormData = async(formData)=>{
+    try {
+      const email = formData.get('email')
+      const password = formData.get('password')
+      const payload = {
+        email,
+        password
+      }
+      login(payload)
+      // formData.reset()
+      router.push('/profile')
+    } catch (error) {
+      console.log(error);
+    }
+  
+  }
+
+
   return (
     <div>
-      
         <h1>page login</h1>
+        <form action={handleFormData}>
+        <ul>
+          <li>
+            {/* <input type="email" ref={email} className='border border-gray-500' /> */}
+            <input type="email" name="email" className='border border-gray-500' />
+          </li>
+          <li>
+            {/* <input type="password" ref={password} className='border border-gray-500' /> */}
+            <input type="password" name="password"  className='border border-gray-500' />
+          </li>
+          <li>
+            <button className='px-4 py-2 rounded-md bg-yellow-300'>Login</button>
+          </li>
+        </ul>
+        </form>
+        
     </div>
   )
 }
--- app/(main-page)/profile/page.js
+++ app/(main-page)/profile/page.js
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client"
+import { getProfile } from '@/service/auth'
+import React, { useEffect, useState } from 'react'
 
 const Profile = () => {
+  const [profile, setProfile] = useState({})
+  const handleGetProfile = async()=>{
+    try {
+      const user = await getProfile()
+      setProfile(user)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(()=>{
+    handleGetProfile()
+  }, [])
   return (
     <div>
         <h1>Profile</h1>
+        <p>{JSON.stringify(profile)}</p>
     </div>
   )
 }
